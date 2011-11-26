deff49021823627ae20a53d271f0ab042dd123aa
prafulla.surve@gmail.com <prafulla.surve@gmail.com@f637d512-7cc2-397b-116a-06ae34797429>
2011-11-26 17:23:29 +0000

    new code that works with everything apart from perp

--- nlp-ie/src/Resources.java
+++
@@ -1,8 +0,0 @@
-
-public final class Resources {
-
-	private final String grammarFile = "/home/akabdul/workspace/nlp-ie/englishPCFG.ser.gz";
-	private final String dictFile = "dictionary/terrorism";
-	
-	
-}
